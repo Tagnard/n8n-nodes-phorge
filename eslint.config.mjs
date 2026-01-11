4ae9501edd6bb458c48cd322a4ab6f11ab5739c2
Emil Haugbergsmyr <emil@raeven.net>
2026-01-11 10:11:10 +0100

    Disable cloud support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,3 @@
-import { config } from '@n8n/node-cli/eslint';
+import { configWithoutCloudSupport } from '@n8n/node-cli/eslint';
 
-config.rules = {
-  ...config.rules,
-  '@n8n/community-nodes/no-restricted-imports': 'off',
-};
-
-export default config;
+export default configWithoutCloudSupport;
